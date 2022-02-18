b84be9fb68c127e7f803ae4f7000a158e6b109e6
ciut1 <williamyeoshuhok1st@gmail.com>
2022-02-18 20:37:14 +1100

    done for the day

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(x, y) {
+    return x + y
+}
+
+function substract(x, y) {
+    return x - y
+}
+
+function multiplication(x, y) {
+    return x * y
+}
+
+function division(x, y) {
+    return x / y
+}
