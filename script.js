9702896ca08a4554da3cb424a5adb73c367b72f6
ciut1 <williamyeoshuhok1st@gmail.com>
2022-02-19 13:10:39 +0900

    too hard

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-function add(x, y) {
-    return x + y
+const display = document.querySelector(".calculator-display")
+const operands = Array.from(document.querySelectorAll(".operands"))
+const operators = Array.from(document.querySelectorAll(".operators"))
+const equal = document.getElementById("equal")
+const decimal = document.getElementById("decimal")
+const plusMinus = document.getElementById("plus-minus")
+const del = document.getElementById("del")
+const clearEntry = document.getElementById("clear-entry")
+const clear = document.getElementById("clear")
+
+// Making the operands (number 0 to 9)
+operands.forEach((operand) => {
+    operand.addEventListener("click", () => {
+        display.textContent += operand.value;
+    })
+})
+// Calculator operation
+function add(a, b) {
+    return a + b
 }
 
-function subtract(x, y) {
-    return x - y
+function subtract(a, b) {
+    return a - b
 }
 
-function multiplication(x, y) {
-    return x * y
+function multiplication(a, b) {
+    return a * b
 }
 
-function division(x, y) {
-    return x / y
+function division(a, b) {
+    return a / b
+}
+
+function operate(a, operator, b) {
+    if (operator === "+") {
+        return add(a, b)
+    } else if (operator === "-") {
+        return subtract(a, b)
+    } else if (operator === "*") {
+        return multiplication(a, b)
+    } else if (operator === "/") {
+        return division(a, b)
+    } else {
+        return alert("Error: Undefined operator")
+    }
 }
